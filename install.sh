ad01468ac9f255eff4ea595d890c3e053cf889c6
Panagiotis Dimopoulos <padi@intracom-telecom.com>
2022-05-13 16:52:04 +0300

    Fix bug in exec name

--- install.sh
+++ install.sh
@@ -16,21 +16,22 @@ is_service_exists() {
 INSTALL_PATH=/opt/backup
 SYSTEMD_PATH=/etc/systemd/system
 EXEC_NAME=backup
+EXEC_FILE=$EXEC_NAME.sh
 SERVICE_FILE=$EXEC_NAME.service
 TIMER_FILE=$EXEC_NAME.timer
 CNF_FILE=mysql.cnf
 
 # Check if needed files exist
-if [ -f $CNF_FILE ] && [ -f $TIMER_FILE ] && [ -f "$EXEC_NAME.sh" ] && [ -f $SERVICE_FILE ]; then
+if [ -f $CNF_FILE ] && [ -f $TIMER_FILE ] && [ -f $EXEC_FILE ] && [ -f $SERVICE_FILE ]; then
     # Check if we upgrade or install for first time
     if is_service_exists "$TIMER_FILE"; then
         systemctl stop $TIMER_FILE
-        cp $EXEC_NAME $INSTALL_PATH
+        cp $EXEC_FILE $INSTALL_PATH
         cp $CNF_FILE $INSTALL_PATH
         systemctl start $TIMER_FILE
     else
         mkdir -p $INSTALL_PATH
-        cp $EXEC_NAME $INSTALL_PATH
+        cp $EXEC_FILE $INSTALL_PATH
         cp $CNF_FILE $INSTALL_PATH
         cp $SERVICE_FILE $SYSTEMD_PATH
         cp $TIMER_FILE $SYSTEMD_PATH
